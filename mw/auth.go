795e3fe007a7ce1bcff10ed54400b456e504b74e
Jon Calhoun <joncalhoun@gmail.com>
2018-05-08 10:35:43 -0400

    Added auth mw

---
+++ mw/auth.go
@@ -0,0 +1,62 @@
+package mw
+
+import (
+	"context"
+	"net/http"
+
+	app "github.com/joncalhoun/widgetapp"
+)
+
+// Auth provides middleware functions for authorizing users and setting the user
+// in the request context.
+type Auth struct {
+	UserService app.UserService
+}
+
+// UserViaSession will retrieve the current user set by the session cookie
+// and set it in the request context. UserViaSession will NOT redirect
+// to the sign in page if the user is not found. That is left for the
+// RequireUser method to handle so that some pages can optionally have
+// access to the current user.
+func (a *Auth) UserViaSession(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, err := r.Cookie("session")
+		if err != nil {
+			// No user found - time to move on
+			next.ServeHTTP(w, r)
+			return
+		}
+		user, err := a.UserService.ByToken(session.Value)
+		if err != nil {
+			// If you want you can retain the original functionality to call
+			// http.Error if any error aside from app.ErrNotFound is returned,
+			// but I find that most of the time we can continue on and let later
+			// code error if it requires a user, otherwise it can continue without
+			// the user.
+			next.ServeHTTP(w, r)
+			return
+		}
+		r.WithContext(context.WithValue(r.Context(), "user", &user))
+		next.ServeHTTP(w, r)
+	}
+}
+
+// RequireUser will verify that a user is set in the request context. It if is
+// set correctly, the next handler will be called, otherwise it will redirect
+// the user to the sign in page and the next handler will not be called.
+func (a *Auth) RequireUser(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmp := r.Context().Value("user")
+		if tmp == nil {
+			http.Redirect(w, r, "/signin", http.StatusFound)
+			return
+		}
+		if _, ok := tmp.(*app.User); !ok {
+			// Whatever was set in the user key isn't a user, so we probably need to
+			// sign in.
+			http.Redirect(w, r, "/signin", http.StatusFound)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
